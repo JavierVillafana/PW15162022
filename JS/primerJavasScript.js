ce5262584d4225f9be8a7cd66798aa2163f4ada3
JavierVillafana <19170704@itculiacan.edu.mx>
2022-11-03 15:40:23 -0700

    JavaScrip - Avance RandomUser

--- JS/primerJavasScript.js
+++ JS/primerJavasScript.js
@@ -1,4 +1,11 @@
 
+async function traerPersona(){
+    const respuesta = await fetch("https://randomuser.me/api/");
+    const datos = await respuesta.json();
+    //console.log(datos);
+    console.log(datos.results[0].name.last+" "+datos.results[0].name.first);
+}
+
 function quitarSITEC(){
     let pregunta = confirm("Segudor de quitar SITEC?");
     if(pregunta == true){
